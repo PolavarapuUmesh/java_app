6dda839c407b6b084290ac440e26cfaf42832008
Polavarapu Umesh <160050560+PolavarapuUmesh@users.noreply.github.com>
2024-06-11 17:14:50 +0530

    Update deploy.sh
    change host IP

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 pwd
 ls
-scp target/*.jar ubuntu@10.0.27.224:/home/ubuntu/
-ssh ubuntu@10.0.27.224 <<EOF
+scp target/*.jar ubuntu@10.0.47.224:/home/ubuntu/
+ssh ubuntu@10.0.47.224 <<EOF
   sudo rm -rf /opt/tomcat/webapps/*.jar
   sudo mv /home/ubuntu/*.jar /opt/tomcat/webapps/
   sudo systemctl restart tomcat

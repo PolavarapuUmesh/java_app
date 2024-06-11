9d045ac07de6bf504dbc0fd93d6c9d1eea21ac91
Polavarapu Umesh <160050560+PolavarapuUmesh@users.noreply.github.com>
2024-06-11 18:08:17 +0530

    Update deploy.sh
    deploy 7

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ pwd
 ssh ubuntu@10.0.47.224 
   scp workspace/subtitle-downloader/target/*.jar ubuntu@10.0.47.224:/home/ubuntu <<EOF
   sudo rm -rf /home/ubuntu/opt/tomcat/webapps/*.jar
-  sudo mv /home/ubuntu/opt/tomcat/webapps/*.jar /var/lib/jenkins/workspace/subtitle-downloader/target/*.jar
+  sudo mv subtitledownloader-1.0-SNAPSHOT.jar /opt/tomcat/webapps
   sudo systemctl restart tomcat
   exit
 EOF

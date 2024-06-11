8e35b16471a508705f2de0452cbc04f13d0c69ed
Polavarapu Umesh <160050560+PolavarapuUmesh@users.noreply.github.com>
2024-06-11 17:19:22 +0530

    Update deploy.sh
    remove

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,1 @@
-pwd
-ls
-scp target/*.jar ubuntu@10.0.47.224:/home/ubuntu/
-ssh ubuntu@10.0.47.224 <<EOF
-  sudo rm -rf /opt/tomcat/webapps/*.jar
-  sudo mv /home/ubuntu/*.jar /opt/tomcat/webapps/
-  sudo systemctl restart tomcat
-  exit
-EOF
-echo "deployed successfully"
+

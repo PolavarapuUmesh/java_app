779bb220192c8899163d2465b8dfdebe15769410
Polavarapu Umesh <160050560+PolavarapuUmesh@users.noreply.github.com>
2024-06-11 17:49:26 +0530

    Update deploy.sh
    practise2

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 ls
 pwd
 ssh ubuntu@10.0.47.224 
-sudo rm -rf /home/ubuntu/opt/tomcat/webapps/subtitledownloader-1.0-SNAPSHOT.jar <<EOF
-  scp workspace/subtitle-downloader/target/subtitledownloader-1.0-SNAPSHOT.jar ubuntu@10.0.47.224:/home/ubuntu
-  sudo mv /home/ubuntu/opt/tomcat/webapps/subtitledownloader-1.0-SNAPSHOT.jar /var/lib/jenkins/workspace/subtitle-downloader/target/subtitledownloader-1.0-SNAPSHOT.jar
+  scp workspace/subtitle-downloader/target/*.jar ubuntu@10.0.47.224:/home/ubuntu <<EOF
+  sudo rm -rf /home/ubuntu/opt/tomcat/webapps/*.jar
+  sudo mv /home/ubuntu/opt/tomcat/webapps/*.jar /var/lib/jenkins/workspace/subtitle-downloader/target/*.jar
   sudo systemctl restart tomcat
   exit
 EOF
